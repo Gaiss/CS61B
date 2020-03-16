64aa61b59294ea58a1508150a57367b6e4d086d3
Yunxiang-Li <46282624+Yunxiang-Li@users.noreply.github.com>
2020-03-16 19:29:02 -0400

    Update NBody.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,116 +1,105 @@
-/**
-    Actually runs our simulation of an universe specified in one of the data files
- */
 public class NBody {
+  /**
+   * Return a double corresponding to the radius of the universe in that file
+   * @param filename a String, indicates the string form of the filename
+   * @return a double, indicates the radius of the universe
+   */
+  public static double readRadius(String filename) {
+    //Create a new In class
+    In in = new In(filename);
+    //Read the first data, an integer indicates the number of planets
+    int planetNum = in.readInt();
+    //Read the second data, a double indicates the radius of the universe
+    double radius = in.readDouble();
+    return radius;
+  }
 
-    private static String backgroundImage = "./images/starfield.jpg"; // background image
-    private static final String backgroundMusic = "./audio/2001.mid"; // background music
-
-    /**
-        Returns a double corresponding to the radius of the universe in given file
-     */
-    public static double readRadius(String s) {
-		In in = new In(s);
-
-        in.readInt();
-        double radius = in.readDouble();
-
-        return radius;
+  /**
+   * Return an array of Planets corresponding to the planets in the file
+   * @param filename a String, indicates the string form of the filename
+   * @return an array of Planets
+   */
+  public static Planet[] readPlanets(String filename) {
+    //Create a new In class
+    In in = new In(filename);
+    //Read the first data, an integer indicates the number of planets
+    int planetNum = in.readInt();
+    //Read the second data, a double indicates the radius of the universe
+    double radius = in.readDouble();
+    //Initialize a new Planet array
+    Planet[] planets = new Planet[planetNum];
+    //Use for loop to initialize each Planet object
+    for(int i = 0; i < planetNum; i++) {
+      //Read each data
+      double xxPos = in.readDouble();
+      double yyPos = in.readDouble();
+      double xxVel = in.readDouble();
+      double yyVel = in.readDouble();
+      double mass = in.readDouble();
+      String imgFileName = in.readString();
+      //Initialize a Planet object according to data read before
+      Planet planet = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+      //Append each Planet object to the array
+      planets[i] = planet;
     }
+    return planets;
+  }
 
-    /**
-        Returns an array of Planets corresponding to the planets defined in given file
-     */
-    public static Planet[] readPlanets(String s) {
-        In in = new In(s);
-
-        int n = in.readInt();
-        in.readDouble();
-
-        Planet[] allPlanets = new Planet[n];
-        for (int i = 0; i < n; i++) {
-            double xP = in.readDouble();
-            double yP = in.readDouble();
-            double xV = in.readDouble();
-            double yV = in.readDouble();
-            double m = in.readDouble();
-            String img = in.readString();
-            allPlanets[i] = new Planet(xP, yP, xV, yV, m, img);
-        }
-
-        return allPlanets;
+  /**
+   * Main function
+   * @param args
+   */
+  public static void main(String[] args) {
+    //Read first and second command line arguments as two double
+    double T = Double.parseDouble(args[0]);
+    double dt = Double.parseDouble(args[1]);
+    //Read third command line argument as a String
+    String filename = args[2];
+    //Call readRadius and readPlanets methods to get radius and the array of Planet objects
+    double radius = readRadius(filename);
+    Planet[] planets = readPlanets(filename);
+    //Draw planets
+    StdDraw.setScale(-radius,radius);
+    // Clears the drawing window//
+    StdDraw.clear();
+    //Prevent flickering in the animation
+    StdDraw.enableDoubleBuffering();
+    //Initialize a double time to zero
+    double time = 0;
+    //loop if time is smaller than T
+    while (time < T) {
+      //Initialize two double array to store net x and y forces for each planet
+      double[] xForces = new double[planets.length];
+      double[] yForces= new double[planets.length];
+      //Calculate each planet's netforce in x and y directions
+      for(int i = 0; i < planets.length; i++) {
+        xForces[i] = planets[i].calcNetForceExertedByX(planets);
+        yForces[i] = planets[i].calcNetForceExertedByY(planets);
+      }
+      //Update each planet's condition
+      for(int j = 0; j < planets.length; j++) {
+        planets[j].update(dt, xForces[j], yForces[j]);
+      }
+      //Draw updated planets
+      StdDraw.picture(0,0,"images/starfield.jpg");
+      //Use for loop to draw each planet
+      for(Planet planet:planets) {
+        planet.draw();
+      }
+      //Show the offscreen buffer
+      StdDraw.show();
+      //Pause the screen for 10 milliseconds
+      StdDraw.pause(10);
+      //Increase the T
+      time += dt;
     }
-
-    public static void main(String[] args) {
-
-        /** Read command line argments */
-        double T = Double.parseDouble(args[0]);
-        double dt = Double.parseDouble(args[1]);
-        String filename = args[2];
-        if (args.length > 3) {
-            NBody.backgroundImage = args[3];
-        }
-
-        /** Read Planets and the universe radius defined in filename */
-        double radius = readRadius(filename);
-        Planet[] allPlanets = readPlanets(filename);
-
-        /** Enables double buffering for smooth animation rendering and prevent it from flickering */
-		StdDraw.enableDoubleBuffering();
-        /** Scale the universe to r */
-        StdDraw.setScale(-radius, radius);
-        /** Clear the drawing window. */
-        StdDraw.clear();
-
-        /** Play the space-audio */
-        StdAudio.play(backgroundMusic);
-
-        /**
-            In this main loop, updating and rendering each Planet's movements with increasing time variable: t
-         */
-        double t = 0.0; // time variable
-        int n = allPlanets.length;
-        double[] xForces = new double[n];
-        double[] yForces = new double[n];
-        while (t < T) {
-
-            /** Calculate net forces for each Planet */
-            for (int i = 0; i < n; i++) {
-                double fX = allPlanets[i].calcNetForceExertedByX(allPlanets);
-                double fY = allPlanets[i].calcNetForceExertedByY(allPlanets);
-                xForces[i] = fX;
-                yForces[i] = fY;
-            }
-
-            /** Update each Planet's members */
-            for (int i = 0; i < n; i++) {
-                allPlanets[i].update(dt, xForces[i], yForces[i]);
-            }
-
-            /* Show the background */
-            StdDraw.picture(0, 0, NBody.backgroundImage);
-
-            /** Draw all of the Planets */
-            for (Planet p : allPlanets) {
-                p.draw();
-            }
-
-            /* Shows the drawing to the screen */
-            StdDraw.show();
-            StdDraw.pause(10);
-
-            t += dt;
-        }
-
-        /**
-            Outputs the final states of each Planet for autograder to work correctly
-         */
-        StdOut.printf("%d\n", n);
-        StdOut.printf("%.2e\n", radius);
-        for (Planet p : allPlanets) {
-            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                          p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
-        }
+    //Print out the whole universe
+    StdOut.printf("%d\n", planets.length);
+    StdOut.printf("%.2e\n", radius);
+    for (int i = 0; i < planets.length; i++) {
+      StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+              planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+              planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
     }
-
+  }
 }
